15f12f4f7ee8e5decdecc95871e95d8dec81b6e8
Baudouin_PORT <baudouin.coupey@protonmail.com>
2019-12-11 13:37:42 +0100

    correction fautes

--- Resa.js
+++ Resa.js
@@ -1,201 +1,200 @@
 class Resa {
-	constructor() {
-		this.formStationNom = document.getElementById('stationNom');
-		this.formStationAdresse = document.getElementById('StationAdresse');
-		this.formStationVelosDispos = document.getElementById('StationVelosDispos');
-		this.compteurText = document.getElementById('compteur');
-		this.canvas = document.getElementById('c1');
-		this.ctx = this.canvas.getContext('2d');
-		//delais de reservation -1 en min
-		this.resaTime = 19;
-	}
+  constructor() {
+    this.formStationNom = document.getElementById('stationNom');
+    this.formStationAdresse = document.getElementById('StationAdresse');
+    this.formStationVelosDispos = document.getElementById('StationVelosDispos');
+    this.compteurText = document.getElementById('compteur');
+    this.canvas = document.getElementById('c1');
+    this.ctx = this.canvas.getContext('2d');
+    //delais de reservation -1 en min
+    // pour 20 mins, mettre 19
+    this.resaTime = 19;
+  }
 
-	initResa(ID) {
-		let urlStation = "https://api.jcdecaux.com/vls/v3/stations/" + ID + "?contract=Lyon&apiKey=" + api_key;
+  initResa(ID) {
+    let urlStation = "https://api.jcdecaux.com/vls/v3/stations/" + ID + "?contract=Lyon&apiKey=" + api_key;
 
-		let element = this;
+    let element = this;
 
-		let data = fetch(urlStation)
-			.then(response => response.json())
-			.then(function (data) {
-				element.formHydrate(data);
-			});
-	}
+    let data = fetch(urlStation)
+      .then(response => response.json())
+      .then(function (data) {
+        element.formHydrate(data);
+      });
+  }
 
-	getStokedName() {
-		this.stockedName = localStorage.getItem('nom');
-		this.stockedFirstName = localStorage.getItem('prenom');
-	}
+  getStokedName() {
+    this.stockedName = localStorage.getItem('nom');
+    this.stockedFirstName = localStorage.getItem('prenom');
+  }
 
-	formHydrate(station) {
-		let nbVelosDipos = station.mainStands.availabilities.bikes;
-		if (nbVelosDipos > 0) {
-			let nom = station.name.split('-')[1];
-			let adresse = station.address;
-			if (adresse == "") {
-				adresse = nom;
-			}
-			document.getElementById('form').style.display = 'block';
-			this.formStationNom.innerText = nom;
-			this.formStationAdresse.innerText = "Adresse : " + adresse;
-			this.formStationVelosDispos.innerText = "Nombre de v√©los diponibles : " + nbVelosDipos;
+  formHydrate(station) {
+    let nbVelosDipos = station.mainStands.availabilities.bikes;
+    if (nbVelosDipos > 0) {
+      let nom = station.name.split('-')[1];
+      let adresse = station.address;
+      if (adresse == "") {
+        adresse = nom;
+      }
+      document.getElementById('form').style.display = 'block';
+      this.formStationNom.innerText = nom;
+      this.formStationAdresse.innerText = "Adresse : " + adresse;
+      this.formStationVelosDispos.innerText = "Nombre de v√É¬©los diponibles : " + nbVelosDipos;
 
-			sessionStorage.setItem('stationName', nom);
-			sessionStorage.setItem('stationAdresse', adresse);
+      sessionStorage.setItem('stationName', nom);
+      sessionStorage.setItem('stationAdresse', adresse);
 
-			this.getStokedName();
+      this.getStokedName();
 
-			if (this.stockedName != "") {
-				let nomStocked = this.stockedName;
-				let prenomSotcked = this.stockedFirstName;
+      if (this.stockedName != "") {
+        let nomStocked = this.stockedName;
+        let prenomSotcked = this.stockedFirstName;
 
-				document.getElementById('Name').value = nomStocked;
-				document.getElementById('FirstName').value = prenomSotcked;
-			}
+        document.getElementById('Name').value = nomStocked;
+        document.getElementById('FirstName').value = prenomSotcked;
+      }
 
 
 
-		} else {
-			let msg = 'Il n\'y a pas de vÈlo disponnible dans cette station.\n\nVeuillez choisir une autre station.';
-			let title = "Pas de vÈlo disponnible";
-			this.displayError(title, msg);
-		}
-	}
+    } else {
+      let msg = 'Il n\'y a pas de v√©lo disponible dans cette station.\n\nVeuillez choisir une autre station.';
+      let title = "Pas de v√©lo disponible";
+      this.displayError(title, msg);
+    }
+  }
 
-	isCanvasBlank(canvas) {
-		const context = canvas.getContext('2d');
+  isCanvasBlank(canvas) {
+    const context = canvas.getContext('2d');
 
-		const pixelBuffer = new Uint32Array(
-			context.getImageData(0, 0, canvas.width, canvas.height).data.buffer
-		);
+    const pixelBuffer = new Uint32Array(
+      context.getImageData(0, 0, canvas.width, canvas.height).data.buffer
+    );
 
-		return !pixelBuffer.some(color => color !== 0);
-	}
+    return !pixelBuffer.some(color => color !== 0);
+  }
 
-	checkForm(e) {
-		let nom = document.getElementById('Name').value;
-		let prenom = document.getElementById('FirstName').value;
-		let canvas = this.ctx;
+  checkForm(e) {
+    let nom = document.getElementById('Name').value;
+    let prenom = document.getElementById('FirstName').value;
+    let canvas = this.ctx;
 
-		let blank = this.isCanvasBlank(this.canvas);
-		if (nom == "" || prenom == "" || blank == true) {
-			this.displayError('Le formulaire est incomplet', 'Veuillez vÈrifier que vous avez bien reseigner votre nom, votre prÈnom et que vous avez signÈ.');
-			e.preventDefault();
-		} else {
-			this.compteur();
-			this.saveName(nom, prenom);
-			e.preventDefault();
-		}
-	}
+    let blank = this.isCanvasBlank(this.canvas);
+    if (nom == "" || prenom == "" || blank == true) {
+      this.displayError('Le formulaire est incomplet', 'Veuillez v√©rifier que vous avez bien reseigner votre nom, votre pr√©nom et que vous avez sign√©.');
+      e.preventDefault();
+    } else {
+      this.compteur();
+      this.saveName(nom, prenom);
+      e.preventDefault();
+    }
+  }
 
-	saveName(nom, prenom) {
-		localStorage.setItem('nom', nom);
-		localStorage.setItem('prenom', prenom);
-	}
+  saveName(nom, prenom) {
+    localStorage.setItem('nom', nom);
+    localStorage.setItem('prenom', prenom);
+  }
 
-	compteur() {
-		let minutesStoked = this.getDate();
-		let minutes;
-		if (minutesStoked == undefined) {
-			minutes = this.resaTime;
-		} else {
-			minutes = minutesStoked;
-		}
-		if (!(sessionStorage.getItem('date'))) {
-			this.setDate();
-		}
-		let secondes = 60;
-		let secondesTxt;
+  compteur() {
+    let minutesStoked = this.getDate();
+    let minutes;
+    if (minutesStoked == undefined) {
+      minutes = this.resaTime;
+    } else {
+      minutes = minutesStoked;
+    }
+    if (!(sessionStorage.getItem('date'))) {
+      this.setDate();
+    }
+    let secondes = 60;
+    let secondesTxt;
 
-		let intervalID = setInterval(plusUneSec, 1000);
+    let intervalID = setInterval(plusUneSec, 1000);
 
-		let element = this;
+    let element = this;
 
-		document.getElementById('displayTimer').style.visibility = 'visible';
-		document.getElementById('form').style.display = 'none';
+    document.getElementById('displayTimer').style.visibility = 'visible';
+    document.getElementById('form').style.display = 'none';
 
-		function plusUneSec() {
-			secondes--;
-			if (minutes == 0 && secondes == 0) {
-				clearInterval(intervalID);
-				element.compteurText.innerText = 'Reservation expir√©e.';
-			} else {
-				if (secondes == 0) {
-					minutes--;
-					secondes = 59;
-				}
-				if (secondes < 10) {
-					secondesTxt = '0' + secondes;
-				} else {
-					secondesTxt = secondes;
-				}
-				element.compteurText.innerText = minutes + ':' + secondesTxt;
-			}
-		}
-	}
+    function plusUneSec() {
+      secondes--;
+      if (minutes == 0 && secondes == 0) {
+        clearInterval(intervalID);
+        element.compteurText.innerText = 'Reservation expir√É¬©e.';
+      } else {
+        if (secondes == 0) {
+          minutes--;
+          secondes = 59;
+        }
+        if (secondes < 10) {
+          secondesTxt = '0' + secondes;
+        } else {
+          secondesTxt = secondes;
+        }
+        element.compteurText.innerText = minutes + ':' + secondesTxt;
+      }
+    }
+  }
 
-	signature() {
-		let draw = false;
-		let ctx = this.ctx;
-		ctx.fillStyle = 'orange';
+  signature() {
+    let draw = false;
+    let ctx = this.ctx;
+    ctx.fillStyle = 'orange';
 
-		let topOffset = this.canvas.getBoundingClientRect();
+    let topOffset = this.canvas.getBoundingClientRect();
 
-		ctx.canvas.addEventListener('mousemove', function (event) {
-			let mouseX = event.x - topOffset.left;
-			let mouseY = event.y - topOffset.top;
-			if (draw == true) {
-				ctx.fillRect(mouseX, mouseY, 4, 4);
-			}
-		});
+    ctx.canvas.addEventListener('mousemove', function (event) {
+      let mouseX = event.x - topOffset.left;
+      let mouseY = event.y - topOffset.top;
+      if (draw == true) {
+        ctx.fillRect(mouseX, mouseY, 4, 4);
+      }
+    });
 
-		ctx.canvas.addEventListener('mouseup', function () {
-			draw = false;
-		});
+    ctx.canvas.addEventListener('mouseup', function () {
+      draw = false;
+    });
 
-		ctx.canvas.addEventListener('mousedown', function () {
-			draw = true;
-		});
-	}
+    ctx.canvas.addEventListener('mousedown', function () {
+      draw = true;
+    });
+  }
 
-	clearCanvas() {
-		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-	}
+  clearCanvas() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
 
-	setDate() {
-		let stockedDate = Date.now();
-		sessionStorage.setItem('date', stockedDate);
-	}
+  setDate() {
+    let stockedDate = Date.now();
+    sessionStorage.setItem('date', stockedDate);
+  }
 
-	getDate() {
-		let stockedDate = Number(sessionStorage.getItem('date'));
-		let actualDate = Date.now();
-		let difference = actualDate - stockedDate;
-		let min;
-		if (difference > 1000 && difference < 1200000) {
-			console.log(difference);
-			min = Math.floor((1200000 - difference) / 60000);
-		}
-		return min;
-	}
+  getDate() {
+    let stockedDate = Number(sessionStorage.getItem('date'));
+    let actualDate = Date.now();
+    let difference = actualDate - stockedDate;
+    let min;
+    if (difference > 1000 && difference < 1200000) {
+      console.log(difference);
+      min = Math.floor((1200000 - difference) / 60000);
+    }
+    return min;
+  }
 
-	checkDate() {
-		let minutesStoked = this.getDate();
-		if (minutesStoked < this.resaTime) {
-			this.formStationNom.innerText = sessionStorage.getItem('stationName');
-			this.formStationAdresse.innerText = "Adresse : " + sessionStorage.getItem('stationAdresse');
-			this.compteur();
-		}
-	}
+  checkDate() {
+    let minutesStoked = this.getDate();
+    if (minutesStoked < this.resaTime) {
+      this.formStationNom.innerText = sessionStorage.getItem('stationName');
+      this.formStationAdresse.innerText = "Adresse : " + sessionStorage.getItem('stationAdresse');
+      this.compteur();
+    }
+  }
 
-	displayError(title, msg) {
-		let modalTitle = document.getElementById('modalTitle');
-		let modalMsg = document.getElementById('modalMsg');
-		modalMsg.innerText = msg;
-		modalTitle.innerText = title;
+  displayError(title, msg) {
+    let modalTitle = document.getElementById('modalTitle');
+    let modalMsg = document.getElementById('modalMsg');
+    modalMsg.innerText = msg;
+    modalTitle.innerText = title;
 
-		$('#myModal').modal('show');
-	}
+    $('#myModal').modal('show');
+  }
 }
-
-//TODO: verifier ce qu'il se passe si on recharge et que la reservation est expirÈe
